e95e15ecc195a25fc00a17d75f7edeb3c26fbf7b
Rahithya <rahithya94@gmail.com>
2025-10-23 09:03:29 +0530

    Change input number for factorial calculation to 5

--- factorial_finder.py
+++ factorial_finder.py
@@ -2,7 +2,7 @@
 factorial = 1
 
 # Initialize the input number to 6
-number = 6
+number = 5
 
 # Loop from 1 to number (inclusive) and multiply factorial by each number
 for i in range(1, number + 1):
